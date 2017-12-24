b64ce62b3a18bbce0b102cf6d060cc74ef7aadce
Sergey Semushin <predelnik@gmail.com>
2017-12-24 18:24:03 +0300

    SDL_mixer update test.

--- cmake/projects/SDL_mixer/hunter.cmake
+++ cmake/projects/SDL_mixer/hunter.cmake
@@ -17,6 +17,17 @@ hunter_add_version(
         b4e97fb430309d5218185afa702f16f0129678be
 )
 
+hunter_add_version(
+        PACKAGE_NAME
+        SDL_mixer
+        VERSION
+        "2.0.2-p1"
+        URL
+        https://github.com/Predelnik/SDL_mixer/archive/2.0.1-hunter.tar.gz
+        SHA1
+        1b723a6c56d387595e9c83a083d6ddde04e85ba1
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(SDL_mixer)
 hunter_download(PACKAGE_NAME SDL_mixer)
